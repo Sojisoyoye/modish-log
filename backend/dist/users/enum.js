aeb60b8f038df91bf07498be88656ccde8539b36
Soji Soyoye <soji.soyoye@gmail.com>
2025-04-04 13:20:01 +0100

    update gitignore

--- backend/dist/users/enum.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UserRole = void 0;
-var UserRole;
-(function (UserRole) {
-    UserRole["Admin"] = "Admin";
-    UserRole["Manager"] = "Manager";
-    UserRole["Salesperson"] = "Salesperson";
-})(UserRole || (exports.UserRole = UserRole = {}));
--- backend/dist/users/utils.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.formatNumber = void 0;
-const formatNumber = (value) => {
-    const num = Number(value);
-    return num % 1 === 0 ? num.toFixed(0) : num.toFixed(1);
-};
-exports.formatNumber = formatNumber;
