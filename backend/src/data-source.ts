1723773443d8d551a57330a25ee95a1bd0062bd2
Soji Soyoye <soji.soyoye@gmail.com>
2025-04-27 22:21:10 +0100

    update data source for prod migration

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -1,19 +1,32 @@
-import { DataSource } from 'typeorm';
-import { Product } from './products/products.entity';
-import { Sale } from './sales/sales.entity';
-import { User } from './users/users.entity';
+import { DataSource } from "typeorm";
+import { Product } from "./products/products.entity";
+import { Sale } from "./sales/sales.entity";
+import { User } from "./users/users.entity";
+import { StockCount } from "./stock/stock-count.entity";
+import { StockBalanceReport } from "./stock/stock-balance-report.entity";
 
 import * as dotenv from "dotenv";
 dotenv.config();
 
+const databaseUrl = process.env.DATABASE_URL;
+
 export const AppDataSource = new DataSource({
-  type: 'postgres',
-  host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT || '5432', 10),
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  entities: [User, Product, Sale],
+  type: "postgres",
+  ...(databaseUrl
+    ? {
+        url: databaseUrl,
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      }
+    : {
+        host: process.env.DB_HOST,
+        port: parseInt(process.env.DB_PORT || "5432", 10),
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
+      }),
+  entities: [User, Product, Sale, StockCount, StockBalanceReport],
   synchronize: false, // Disable synchronize in production
-  migrations: ['src/migrations/*.ts'], // Path to your migration files
+  migrations: ["src/migrations/*.ts"], // Path to your migration files
 });
