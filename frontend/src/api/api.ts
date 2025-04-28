58312cf183113837de2d61d73d071e03e53753d5
Soji Soyoye <soji.soyoye@gmail.com>
2025-04-28 09:38:38 +0100

    update frontend api

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -9,7 +9,7 @@ import {
 
 const API_BASE_URL = process.env.REACT_APP_API_URL;
 const api = axios.create({
-  baseURL: API_BASE_URL,
+  baseURL: `${API_BASE_URL}/api`,
 });
 
 api.interceptors.request.use((config) => {
